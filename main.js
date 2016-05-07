c27885ce27ec78924fab003d82f44f53d90ed877
Loganator4625 <loganjacavanah@gmail.com>
2016-05-07 10:59:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+var kingdomName = " "
 
+
+function startGame() {
+  var GameStartPrompt = prompt("Welcome! Please Choose a Name For Your Kingdom to Begin!");
+  kingdomName = GameStartPrompt;
+  
+}
