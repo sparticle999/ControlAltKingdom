4976f8a5bffd499e020234137893426318afee81
Loganator4625 <loganjacavanah@gmail.com>
2016-05-07 11:20:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ function startGame() {
   var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your Kingdom to Begin!");
   kingdomName = gameStartPrompt;
   document.getElementById("kingdomName").innerHTML = kingdomName;
+  startButton = class("hidden");
   
 }
