9a7380a693418e1a89f02c5ddb501529dd571b20
Loganator4625 <loganjacavanah@gmail.com>
2016-05-08 20:09:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var money = 0;
 var upgradeGrainPricePrice = 20;
 var grainPriceLevel = 1;
 var upgrade2 = true;
+var upgrade3 = true;
 
 function startGame() {
   var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your Kingdom to Begin!");
@@ -61,6 +62,13 @@ function upgradeGrainYield() {
   }
 }
 function upgradeGrainPrice() {
+  if(upgrade3 === true) {
+    if(money >= 300){
+      upgrade3 = false;
+      alert("Your Town is growing rapidly! your numbers are increasing and it is time that some policies were enacted!");
+      document.getElementById("goToResearch").className = " ";
+    }
+  }
   if(money >= upgradeGrainPricePrice) {
     money -= upgradeGrainPricePrice;
     upgradeGrainPricePrice = Math.floor(upgradeGrainPricePrice * 1.5);
@@ -70,4 +78,15 @@ function upgradeGrainPrice() {
     document.getElementById("grainSellPrice").innerHTML = grainSellprice;
     document.getElementById("upgradeGrainPricePrice").innerHTML = upgradeGrainPricePrice;
   }
+}
+function goToResearch() {
+  document.getElementById("moneyBox").className = "hidden";
+  document.getElementById("grainButton").className = "hidden";
+  document.getElementById("grainSellButton").className = "hidden";
+  document.getElementById("grainStore").className = "hidden";
+  document.getElementById("grainYieldUpgrade").className = "hidden";
+  document.getElementById("welcomingHeading").className = "hidden";
+  document.getElementById("yieldLevel").className = "hidden";
+  document.getElementById("grainPriceUpgrade").className = "hidden";
+  document.getElementById("researchButton").className = "hidden";
 }
